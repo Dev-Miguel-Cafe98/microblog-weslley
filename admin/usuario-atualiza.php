01748aa95bba2491d6a604528ab7f54e64ae2300
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-11-19 15:28:07 -0300

    Update usuario-atualiza.php

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -10,9 +10,10 @@ $dadosUsuario = ListarUmUsuario($conexao, $id);
 
 ?>
 
-<pre>
-<?=var_dump($dadosUsuario)?>
-</pre>
+<!-- Para comentar e verificar as string comando para progamador
+ 
+var_dump($dadosUsuario) -->
+
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
@@ -25,17 +26,17 @@ $dadosUsuario = ListarUmUsuario($conexao, $id);
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?= $dadosUsuario['nome'] ?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?= $dadosUsuario['email'] ?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="senha">Senha:</label>
-				<input class="form-control" type="password" id="senha" name="senha" placeholder="Preencha apenas se for alterar">
+				<input  class="form-control" type="password" id="senha" name="senha" placeholder="Preencha apenas se for alterar">
 			</div>
 
 			<div class="mb-3">
