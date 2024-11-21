66b64b60db3e9af63916b3368ca5e376de0783fd
Welley Miguel <158380657+Dev-Miguel-Cafe98@users.noreply.github.com>
2024-11-21 14:43:40 -0300

    Create funcoes-controle-de-acesso.php

---
+++ includes/funcoes-controle-de-acesso.php
@@ -0,0 +1,34 @@
+<?php
+// funcoes-controle-de-acesso.php
+
+/* Sessões (SESSION) no PHP  Funcionalidade  usada para o controle de acesso à determinadas páginas e/ou recurso do site
+
+Exemplo: área administrativa , painel de controle, área de cliente/aluno etc.
+
+Nesta áreas o acesso se dá através de alguma forma de autenticação. 
+Exemplo: login/senha, biometria, facial , 2-fatores etc ..
+
+*/
+
+
+/*  Verificando se NÃO EXISTE uma sessão em andamento */
+if (!isset($_SESSION)) {
+    // se não tiver, então iniciamos uma sessão
+    session_start();
+}
+
+/* Proteger o acesso às páginas da área administrativa */
+function verificarAcesso()
+{
+    /* se NÃO EXISTIR uma variável de sessão chamada 'id' então significa que o usuário não está logado */
+    if (!isset($_SESSION['id'])) {
+        /* Portanto, destruímos qualquer resquício de sessão */
+        session_destroy();
+
+        /* Fazemos o usuário ir para página de login.php */
+        header("Location:../login.php?acesso_negado");
+
+        /* E paramos completamente a execução do script */
+        die();
+    }
+}
